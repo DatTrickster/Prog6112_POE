a0b3f8eb9c0811d0f507da9a5b2af5db65d649c3
Ziyaad Mahomed <108807317+DatTrickster@users.noreply.github.com>
2023-09-21 15:40:01 +0200

    Update CharacterBP.java

--- src/justfight/CharacterBP.java
+++ src/justfight/CharacterBP.java
@@ -4,29 +4,35 @@
  */
 package justfight;
 
+// Base class for characters in the game
 public class CharacterBP {
-    private String damageType;
-    private String characterClass;
-    private int health;
+    private String damageType;    // The type of damage the character can inflict
+    private String characterClass; // The class of the character
+    private int health;           // The current health of the character
 
+    // Constructor to initialize a character with damage type and character class
     public CharacterBP(String damageType, String characterClass) {
-        this.damageType = damageType;
-        this.characterClass = characterClass;
+        this.damageType = damageType;       // Set the damage type
+        this.characterClass = characterClass; // Set the character class
         this.health = 1000; // Set initial health to 1000 HP
     }
 
+    // Getter method to retrieve the damage type of the character
     public String getDamageType() {
         return damageType;
     }
 
+    // Getter method to retrieve the character class
     public String getCharacterClass() {
         return characterClass;
     }
 
+    // Getter method to retrieve the current health of the character
     public int getHealth() {
         return health;
     }
 
+    // Method to reduce the character's health by a specified amount (damage)
     public void takeDamage(int damage) {
         health -= damage;
         if (health < 0) {
@@ -34,12 +40,15 @@ public class CharacterBP {
         }
     }
 
+    // Method to check if the character is alive (health greater than 0)
     public boolean isAlive() {
         return health > 0;
     }
 
+    // Override the toString method to provide a string representation of the character
     @Override
     public String toString() {
         return "Damage Type = " + damageType + ", Class = " + characterClass + ", Health = " + health;
     }
 }
+
