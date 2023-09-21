e9db467d7e3ea944d578e659d5a302fb7150ca25
Ziyaad Mahomed <108807317+DatTrickster@users.noreply.github.com>
2023-09-21 15:36:30 +0200

    added comments

--- src/prog6112_assignment/OBJStudent.java
+++ src/prog6112_assignment/OBJStudent.java
@@ -4,38 +4,45 @@
  */
 package prog6112_assignment;
 
-
+// A class to represent a student object
 public class OBJStudent {
-    private final int id;
-    private final String name;
-    private final int age;
-    private final String email;
-    private final String course;
+    // Private fields to store student information
+    private final int id;      // Student ID
+    private final String name; // Student name
+    private final int age;     // Student age
+    private final String email; // Student email
+    private final String course; // Student course
 
+    // Constructor to initialize a student object with provided information
     public OBJStudent(int id, String name, int age, String email, String course) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.email = email;
-        this.course = course;
+        this.id = id;       // Set the student ID
+        this.name = name;   // Set the student name
+        this.age = age;     // Set the student age
+        this.email = email; // Set the student email
+        this.course = course; // Set the student course
     }
 
+    // Getter method to retrieve the student ID
     public int getId() {
         return id;
     }
 
+    // Getter method to retrieve the student name
     public String getName() {
         return name;
     }
 
+    // Getter method to retrieve the student age
     public int getAge() {
         return age;
     }
 
+    // Getter method to retrieve the student email
     public String getEmail() {
         return email;
     }
 
+    // Getter method to retrieve the student course
     public String getCourse() {
         return course;
     }
