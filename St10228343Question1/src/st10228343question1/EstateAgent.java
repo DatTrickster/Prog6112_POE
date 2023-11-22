597b790ff08e172d1b08707462968016efc0952e
Ziyaad Mahomed <108807317+DatTrickster@users.noreply.github.com>
2023-11-22 16:03:17 +0000

    Update EstateAgent.java

--- St10228343Question1/src/st10228343question1/EstateAgent.java
+++ St10228343Question1/src/st10228343question1/EstateAgent.java
@@ -5,13 +5,19 @@
 package st10228343question1;
 
 /**
- *
+ * Implementation of the IEstateAgent interface representing an Estate Agent.
+ * This class provides methods to calculate estate agent sales, commission, and find the top-performing estate agent.
+ * 
  * @author Extinction
  */
-
 public class EstateAgent implements IEstateAgent {
 
-    // Calculate total sales for an estate agent
+    /**
+     * Calculates the total sales for an estate agent based on monthly property sales.
+     * 
+     * @param propertySales an array containing monthly property sales
+     * @return the total sales for the estate agent
+     */
     @Override
     public double estateAgentSales(double[] propertySales) {
         double totalSales = 0;
@@ -21,13 +27,23 @@ public class EstateAgent implements IEstateAgent {
         return totalSales;
     }
 
-    // Calculate 2% commission for an estate agent
+    /**
+     * Calculates the commission earned by an estate agent based on total property sales.
+     * 
+     * @param propertySales the total property sales for the estate agent
+     * @return the commission earned by the estate agent
+     */
     @Override
     public double estateAgentCommission(double propertySales) {
         return 0.02 * propertySales;
     }
 
-    // Find the top-performing estate agent
+    /**
+     * Finds the top-performing estate agent based on an array of total sales for multiple agents.
+     * 
+     * @param totalSales an array containing total sales for multiple estate agents
+     * @return the index of the top-performing estate agent
+     */
     @Override
     public int topEstateAgent(double[] totalSales) {
         int topAgent = 0;
@@ -43,3 +59,9 @@ public class EstateAgent implements IEstateAgent {
         return topAgent;
     }
 }
+
+/*
+Code reference:
+[solved] Q11 contain a two-dimensional array to contain three property - software development (IPMA6212) (no date) Studocu. 
+Available at: https://www.studocu.com/en-za/messages/question/4655379/q11-contain-a-two-dimensional-array-to-contain-three-property-sales-for-january-february-and (Accessed: 22 November 2023).
+*/
