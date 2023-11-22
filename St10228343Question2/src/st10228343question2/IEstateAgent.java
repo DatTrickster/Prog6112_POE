9b943e30fd657833f16521c9fdd86848a5fe6762
Ziyaad Mahomed <108807317+DatTrickster@users.noreply.github.com>
2023-11-22 16:22:09 +0000

    Update IEstateAgent.java

--- St10228343Question2/src/st10228343question2/IEstateAgent.java
+++ St10228343Question2/src/st10228343question2/IEstateAgent.java
@@ -4,11 +4,40 @@
  */
 package st10228343question2;
 
-
+/**
+ * Interface defining methods for calculating commission and validating estate agent data.
+ */
 public interface IEstateAgent {
+
+    /**
+     * Calculates the commission based on the given property price and commission percentage.
+     *
+     * @param propertyPrice         The price of the property.
+     * @param commissionPercentage  The commission percentage.
+     * @return                      The calculated commission.
+     */
     double calculateCommission(double propertyPrice, double commissionPercentage);
 
+    /**
+     * Validates estate agent data, checking for empty agent name, non-positive property price,
+     * and invalid commission percentage.
+     *
+     * @param agentName             The name of the estate agent.
+     * @param propertyPrice         The price of the property.
+     * @param commissionPercentage  The commission percentage.
+     * @return                      A validation error message or null if no errors.
+     */
     String validateData(String agentName, double propertyPrice, double commissionPercentage);
 
-    public String validateData(String invalidLocation, String agentName, double propertyPrice, double commissionPercentage);
+    /**
+     * Placeholder method for validating estate agent data with location (unused).
+     * Throws UnsupportedOperationException.
+     *
+     * @param invalidLocation       Placeholder for location validation (unused).
+     * @param agentName             The name of the estate agent.
+     * @param propertyPrice         The price of the property.
+     * @param commissionPercentage  The commission percentage.
+     * @return                      Validation error message (unsupported).
+     */
+    String validateData(String invalidLocation, String agentName, double propertyPrice, double commissionPercentage);
 }
