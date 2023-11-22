75b570ac8e08e73d25a54541c1b9d6552f9022d6
Ziyaad Mahomed <108807317+DatTrickster@users.noreply.github.com>
2023-11-22 17:10:27 +0000

    Update IEstateAgent.java

--- St10228343Question1/src/st10228343question1/IEstateAgent.java
+++ St10228343Question1/src/st10228343question1/IEstateAgent.java
@@ -2,16 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package st10228343question1;
 
 /**
  * Interface for Estate Agents.
  * This interface defines methods for calculating estate agent sales, commission, and finding the top-performing estate agent.
+ * Each method represents a specific functionality related to estate agent performance metrics.
  * 
- * @author Extinction
+ * @author Your Name
  */
 public interface IEstateAgent {
-    
+
     /**
      * Calculates the total sales for an estate agent based on monthly property sales.
      * 
@@ -36,9 +38,3 @@ public interface IEstateAgent {
      */
     int topEstateAgent(double[] totalSales);
 }
-
-/*
-Code reference:
-[solved] Q11 contain a two-dimensional array to contain three property - software development (IPMA6212) (no date) Studocu. 
-Available at: https://www.studocu.com/en-za/messages/question/4655379/q11-contain-a-two-dimensional-array-to-contain-three-property-sales-for-january-february-and (Accessed: 22 November 2023).
-*/
