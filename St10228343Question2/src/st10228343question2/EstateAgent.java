ea72984fe8d0bf6a34898e6465490bc0cbaa5db1
Ziyaad Mahomed <108807317+DatTrickster@users.noreply.github.com>
2023-11-22 16:20:47 +0000

    Update EstateAgent.java

--- St10228343Question2/src/st10228343question2/EstateAgent.java
+++ St10228343Question2/src/st10228343question2/EstateAgent.java
@@ -4,12 +4,33 @@
  */
 package st10228343question2;
 
+/**
+ * Implementation of the IEstateAgent interface providing methods for calculating commission
+ * and validating estate agent data.
+ */
 public class EstateAgent implements IEstateAgent {
+
+    /**
+     * Calculates the commission based on the given property price and commission percentage.
+     *
+     * @param propertyPrice         The price of the property.
+     * @param commissionPercentage  The commission percentage.
+     * @return                      The calculated commission.
+     */
     @Override
     public double calculateCommission(double propertyPrice, double commissionPercentage) {
         return propertyPrice * (commissionPercentage / 100);
     }
 
+    /**
+     * Validates estate agent data, checking for empty agent name, non-positive property price,
+     * and invalid commission percentage.
+     *
+     * @param agentName             The name of the estate agent.
+     * @param propertyPrice         The price of the property.
+     * @param commissionPercentage  The commission percentage.
+     * @return                      A validation error message or null if no errors.
+     */
     @Override
     public String validateData(String agentName, double propertyPrice, double commissionPercentage) {
         if (agentName == null || agentName.trim().isEmpty()) {
@@ -27,8 +48,18 @@ public class EstateAgent implements IEstateAgent {
         return null; // No validation errors
     }
 
+    /**
+     * Placeholder method for validating estate agent data with location (unused).
+     * Throws UnsupportedOperationException.
+     *
+     * @param invalidLocation       Placeholder for location validation (unused).
+     * @param agentName             The name of the estate agent.
+     * @param propertyPrice         The price of the property.
+     * @param commissionPercentage  The commission percentage.
+     * @return                      Validation error message (unsupported).
+     */
     @Override
     public String validateData(String invalidLocation, String agentName, double propertyPrice, double commissionPercentage) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
