a78ff26035a75eb55dc201c8eaa73e4332c5bac3
Ziyaad Mahomed <108807317+DatTrickster@users.noreply.github.com>
2023-11-22 16:24:11 +0000

    Update EstateAgentCommissionApp.java

--- St10228343Question2/src/st10228343question2/EstateAgentCommissionApp.java
+++ St10228343Question2/src/st10228343question2/EstateAgentCommissionApp.java
@@ -230,8 +230,14 @@ public class EstateAgentCommissionApp extends JFrame {
 
 /*
 
-Interfaces (no date) Interfaces (The JavaTM Tutorials &gt; Learning the Java Language &gt; Interfaces and Inheritance).
-Available at: https://docs.oracle.com/javase/tutorial/java/IandI/createinterface.html (Accessed: 22 November 2023). 
+Interfaces (no date) Interfaces (The JavaTM Tutorials &gt; Learning the Java Language &gt; Interfaces and Inheritance).Available at: https://docs.oracle.com/javase/tutorial/java/IandI/createinterface.html (Accessed: 22 November 2023). 
 
+Different ways to declare and initialize 2-D array in Java (2023) GeeksforGeeks. Available at: https://www.geeksforgeeks.org/different-ways-to-declare-and-initialize-2-d-array-in-java/ (Accessed: 22 November 2023). 
+
+Interfaces (no date) Interfaces (The JavaTM Tutorials &gt; Learning the Java Language &gt; Interfaces and Inheritance). Available at: https://docs.oracle.com/javase/tutorial/java/IandI/createinterface.html (Accessed: 22 November 2023). 
+
+John MulaneyJohn Mulaney&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 10999 bronze badges and Mạnh Quyết NguyễnMạnh Quyết Nguyễn&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 17.8k11 gold badge2323 silver badges5151 bronze badges (1964) How to create an interface based on specific requirements, Stack Overflow. Available at: https://stackoverflow.com/questions/50543702/how-to-create-an-interface-based-on-specific-requirements (Accessed: 22 November 2023). 
+
+[solved] Q11 contain a twodimensional array to contain three property - software development (IPMA6212) (no date) Studocu. Available at: https://www.studocu.com/en-za/messages/question/4655379/q11-contain-a-two-dimensional-array-to-contain-three-property-sales-for-january-february-and (Accessed: 22 November 2023). 
 
 */
